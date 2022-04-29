26584c9022aa1446a2615eb5b049f6602d2a611d
Yan Siegers <hello@yansiegers.nl>
2022-04-29 12:51:16 +0200

    Rebuild RoR project without Docker

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Myapp
+module Equiville
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
